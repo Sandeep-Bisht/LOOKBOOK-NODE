4db37d7b3b30f378a3e67ebc8fd97880145f7771
SHUBHAM-SEMWAL <87406338+SHUBHAM-SEMWAL@users.noreply.github.com>
2023-12-08 11:30:29 +0530

    create get artist request api

--- controllers/Users/routes.js
+++ controllers/Users/routes.js
@@ -3,10 +3,12 @@ const router = require("express").Router()
 
 const { getAllUsers } = require('./getAllUsers')
 const { setProfile, getProfile } = require('./profile')
+const { getArtistRequests } = require('./artistsRequest')
 const { upload } = require("../../config/upload")
 
 router.get('/get-all-users',passport.authenticate('jwt',{session:false}),getAllUsers)
 router.post('/setProfile',passport.authenticate('jwt',{session:false}),upload.any('files'),setProfile)
 router.get('/getProfile',passport.authenticate('jwt',{session:false}),getProfile)
+router.get('/getArtistRequests',passport.authenticate('jwt',{session:false}),getArtistRequests)
 
 module.exports = router
