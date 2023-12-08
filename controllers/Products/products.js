c9cbf963ae510d9f5ac0154839e5202b72fb1c38
Deepak Negi <deepak.negi.giksindia.com>
2023-12-08 22:04:46 +0530

    add get product by id api on product controller

--- controllers/Products/products.js
+++ controllers/Products/products.js
@@ -70,5 +70,80 @@ module.exports={
         message:"Somthing went wrong"
       })
     }
-  }
+  },
+
+  get_product_by_id: async (req, res) => {
+    const { _id } = req.body;
+    try {  
+      await Products.findById(_id).then((result)=>
+      {
+        if (result !== null) {
+          res.status(200).json({
+            error: false,
+            message: "Data found",
+            data: result,
+          });
+        } else {
+          res.status(404).json({
+            error: true,
+            message: "Data not found",
+          });
+        }
+      } ) 
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({
+        error: true,
+        message: "Internal server error",
+      });
+    }
+  },
+
+  update_product:async(req,res)=>{
+    let data = {};
+  
+    const { title, _id } = req.body;
+    data = {
+      _id: _id,
+      title: title,
+    };
+  
+    if (req.files) {
+      try {
+        let fileUploadResponse = await uploadFilesToImagekit(req);
+        if (fileUploadResponse && fileUploadResponse.length > 0) {
+          let icon = fileUploadResponse.find((item) => item.fieldName == "updatedIcon");
+          if (icon) data = { ...data, icon: icon.response };
+          let image = fileUploadResponse.find((item) => item.fieldName == 'updatedImage');
+          if (image) data = { ...data, image: image.response };
+        }
+      } catch (uploadError) {
+        return res.status(500).json({
+          error: true,
+          message: "Error uploading files.",
+        });
+      }
+    }
+  
+    try {
+      const result = await Products.findByIdAndUpdate(_id, data);
+      if (result) {
+        return res.status(200).json({
+          error: false,
+          message: "Products updated successfully",
+        });
+      } else {
+        return res.status(400).json({
+          error: true,
+          message: "Error updating Products",
+        });
+      }
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Something went wrong, please try again later.",
+      });
+    }
+}
+
 }
