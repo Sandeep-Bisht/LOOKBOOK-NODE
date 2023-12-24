7cbb932acbb87facb2883499957f3eecb6542d11
SHUBHAM-SEMWAL <87406338+SHUBHAM-SEMWAL@users.noreply.github.com>
2023-12-24 09:07:32 +0530

    restore the routes file with resolved error

--- server.js
+++ server.js
@@ -6,14 +6,9 @@ const app = express();
 require('./config/connection');
 
 const port = process.env.PORT || 8080;
+const routes = require('./routes/routes');
 
 const passport = require('passport');
-
-const AuthRoutes = require('./controllers/Auth/routes')
-const UsersRoutes = require('./controllers/Users/routes');
-const ServicesRoutes = require('./controllers/Services/routes');
-const ProductsRoutes = require('./controllers/products/routes')
-const BlogsRoutes = require('./controllers/Blog/routes')
 
 app.use(cors({origin:true}));
 app.use(express.json());
@@ -23,11 +18,7 @@ app.use(passport.initialize())
 const JwtStrategy = require('./middleware/passport-jwt')
 JwtStrategy(passport);
 
-app.use('/api/auth',AuthRoutes);
-app.use('/api/users',UsersRoutes);
-app.use('/api/service',ServicesRoutes);
-app.use('/api/product',ProductsRoutes)
-app.use('/api/blog',BlogsRoutes);
+app.use('/api',routes);
 
 app.listen(port , ()=>{
     console.log('Server running on port ' + port);
