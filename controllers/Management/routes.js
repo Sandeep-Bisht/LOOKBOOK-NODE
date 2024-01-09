1048168249afceefe5cb2c55a31df4f5c9b59f15
Sandeep Bisht <>
2024-01-09 11:33:50 +0530

    count services

--- controllers/Management/routes.js
+++ controllers/Management/routes.js
@@ -1,11 +1,13 @@
 const passport = require("passport")
 const router = require("express").Router()
 
-const { verifyRequest, artistRequestById, markFeaturedArtist } = require('./artist-requests/manage-artist-requests')
+const { verifyRequest, artistRequestById, markFeaturedArtist } = require('./artist-requests/manage-artist-requests');
+const { getAdminDashboardInitialData } = require('./adminDashboardData/admin-dashboard-data')
 
 router.post('/artist-request',passport.authenticate('jwt',{session:false}),verifyRequest)
 router.get('/artist-request-by-id/:request_id',passport.authenticate('jwt',{session:false}),artistRequestById);
 router.post('/mark-featured-artist', passport.authenticate('jwt', {session:false}), markFeaturedArtist);
+router.get('/get-admin-dashboard-initial-data', passport.authenticate('jwt', {session:false}), getAdminDashboardInitialData)
 
 
 module.exports = router
