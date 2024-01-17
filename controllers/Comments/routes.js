93f0b7f15a652e93eebf71f66a9a652200c5b032
Deepak Negi <deepak.negi.giksindia.com>
2024-01-17 16:54:27 +0530

    some changes inside get_blog_by_slug api fot getting the all approved commetns

--- controllers/Comments/routes.js
+++ controllers/Comments/routes.js
@@ -1,4 +1,4 @@
-
+const passport = require("passport")
 const router = require("express").Router()
 
 const  comments  = require('./comments')
@@ -6,5 +6,5 @@ const  comments  = require('./comments')
 router.post('/comments-create',comments.create)
 router.get('/all_comments',comments.getAllComments);
 router.delete('/delete_comments_by_id/:comment_id',comments.deleteCommentById)
+router.put('/update_comments',passport.authenticate('jwt',{session:false}),comments.updateCommentById)
 module.exports = router
- 
