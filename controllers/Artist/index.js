48c6fcc2522a20aa74112b58c41d3594306feb19
Sandeep Bisht <>
2024-01-18 12:06:10 +0530

    update artist pricing

--- controllers/Artist/index.js
+++ controllers/Artist/index.js
@@ -60,6 +60,8 @@ exports.get_artist_pricing = async (req, res) => {
       }
     });
 
+    
+
     return res.status(200).json({
       error: false,
       pricing: artistPricing,
@@ -71,3 +73,56 @@ exports.get_artist_pricing = async (req, res) => {
     });
   }
 };
+
+exports.updatePricing = async (req, res) => {
+  try {
+    let user_id = req.user._id;
+
+    let { serviceId, price, sessionTime, totalPrice } = req.body;
+
+    const artist = await Artists.findOne({ user_id });
+
+    if (!artist) {
+      return res.status(400).json({
+        error: true,
+        message: "Your artist data is not available.",
+      });
+    }
+    
+    let serviceIndex = artist.pricing.findIndex((el) => el.service.equals(serviceId));
+
+    if (serviceIndex !== -1) {
+      // Update the properties of the found service in the array
+      let updatedPricingData = artist.pricing;
+      updatedPricingData[serviceIndex].price = price;
+      updatedPricingData[serviceIndex].totalPrice = totalPrice;
+      updatedPricingData[serviceIndex].sessionTime = sessionTime;     
+
+
+      // to update perticular field in model use findOneAndUpdate
+
+      let artistData = await Artists.findOneAndUpdate(
+        { user_id},
+        { $set: {'pricing':updatedPricingData} },
+        { new: true }
+      )
+
+      return res.status(200).json({
+        error: false,
+        pricing: artistData.pricing,
+      });
+    } else {
+      return res.status(404).json({
+        error: true,
+        message: "Service not found.",
+      });
+    }
+  } catch (error) {
+    console.error("Error in updating Pricing:", error);
+    res.status(500).json({
+      error: true,
+      message: "Internal Server Error.",
+    });
+  }
+};
+
