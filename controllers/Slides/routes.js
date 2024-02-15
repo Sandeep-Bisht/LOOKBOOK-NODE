b442bfbe68b3d8b44340829763cf3144bc41dd2e
Deepak Negi <deepak.negi.giksindia.com>
2024-02-15 15:52:44 +0530

    change the folder and file name slider to slide

--- controllers/Slides/routes.js
+++ controllers/Slides/routes.js
@@ -7,6 +7,6 @@ const { upload } = require('../../config/upload')
 router.post('/slides-create',passport.authenticate('jwt',{session:false}),upload.any('files'),slides.create)
 router.get('/all_slides',slides.get_all_slides);
 router.put('/slides_update',passport.authenticate('jwt',{session:false}),upload.any('files'),slides.update_slides);
-router.post('/get_slides_by_id/:_id',passport.authenticate('jwt',{session:false}),slides.get_slides_by_id)
+router.get('/get_slides_by_id/:_id',passport.authenticate('jwt',{session:false}),slides.get_slides_by_id)
 
 module.exports = router
