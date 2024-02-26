fa88e80ced0a5f6275a721ff0027ab060e3c0124
Deepak Negi <deepak.negi.giksindia.com>
2024-02-26 18:16:40 +0530

    make api for addcertificates

--- controllers/Users/routes.js
+++ controllers/Users/routes.js
@@ -2,7 +2,7 @@ const passport = require("passport")
 const router = require("express").Router()
 
 const { setProfile, getProfile } = require('./profile')
-const { getArtistRequests, updateArtistRequest, getAllArtistRequest } = require('./artistsRequest')
+const { getArtistRequests, updateArtistRequest, getAllArtistRequest, addCertificates } = require('./artistsRequest')
 const { upload } = require("../../config/upload")
 
 router.post('/setProfile',passport.authenticate('jwt',{session:false}),upload.any('files'),setProfile)
@@ -10,6 +10,7 @@ router.get('/getProfile',passport.authenticate('jwt',{session:false}),getProfile
 
 router.get('/getArtistRequests',passport.authenticate('jwt',{session:false}),getArtistRequests)
 router.post('/updateArtistRequest',passport.authenticate('jwt',{session:false}),upload.any('files'),updateArtistRequest)
+router.post('/artist-request/addCertificates',passport.authenticate('jwt',{session:false}),upload.any('files'),addCertificates)
 router.get('/getAllArtistRequest',passport.authenticate('jwt',{session:false}),getAllArtistRequest)
 
 
